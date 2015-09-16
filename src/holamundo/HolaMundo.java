27a7a1bf92ddba31428fbb84303fa8a3b1115b90
usuario <usuario@10.22.143.32>
2015-09-16 09:56:16 +0100

    Tercera versión

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,7 +4,9 @@
  */
 package holamundo;
 
-import java.util.Date;
+import java.util.Calendar;
+//import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -18,7 +20,10 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");// TODO code application logic here
         Person p1;
-        p1 = new Person("Jose Miguel", "Calvo", new Date(93,10,6));
+        Calendar f1;
+        f1 = GregorianCalendar.getInstance();
+        f1.set(1993,9,6);
+        p1 = new Person("Jose Miguel", "Calvo", f1);
         System.out.println(p1.getBirthday());
         System.out.println(p1.getFullName());
         System.out.println(p1.getAge());
