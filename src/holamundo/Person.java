fbb2680a78c98a8e0cb977f9c256d98d360bb098
usuario <usuario@10.22.143.32>
2015-09-16 09:44:04 +0100

    Segunda versión

--- src/holamundo/Person.java
+++ src/holamundo/Person.java
@@ -14,6 +14,7 @@ public class Person {
     private final String name;
     private final String surname;
     private final Date birthday;
+    private static final long MILLESECOND_PER_YEAR = (long) (1000*60*60*24*365.25);
 
     public Person(String name, String surname, Date birthday) {
         this.name = name;
@@ -39,6 +40,11 @@ public class Person {
     
     public int getAge () {
         Date today = new Date();
-        return (int) ((today.getTime()-birthday.getTime())/(1000*60*60*24*365.25));
+        return (int) (getMilis(today.getTime()-birthday.getTime()));
+    }
+    
+    private long getMilis(long milli) {
+        return milli/MILLESECOND_PER_YEAR;
+        
     }
 }
